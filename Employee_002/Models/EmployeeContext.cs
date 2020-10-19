306e4a392ae3dd330b8a2f505fe72b956a8c2f83
NaufalHaryanda <naufalharyanda10@gmail.com>
2020-10-19 12:17:54 +0700

    Add project files.

---
+++ Employee_002/Models/EmployeeContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Employee_002.Models
+{
+    public class EmployeeContext : DbContext
+
+    {
+        public EmployeeContext(DbContextOptions<EmployeeContext> options) :
+ base(options)
+        { }
+        public DbSet<Employees> Employees { get; set; }
+    }
+}
